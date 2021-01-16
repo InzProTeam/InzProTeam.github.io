ca13a0bb1808dd5abc65aad54224f1a358f95128
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2021-01-16 19:36:49 +0100

    Add files via upload

--- WebFormTetris.aspx.cs
+++ WebFormTetris.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,14 +12,86 @@ namespace InzProWeb
 {
     public partial class WebFormTetris : System.Web.UI.Page
     {
+        readonly string _strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            Session["role"] = "user";
+            Session["username"] = "siema";
+            if (string.IsNullOrEmpty((string)Session["role"]))
+            {
+                TextBoxScore.Visible = false;
+                ButtonSave.Visible = false;
+            }
+            else if (Session["role"].Equals("user"))
+            {
+                TextBoxScore.Visible = true;
+                ButtonSave.Visible = true;
+            }
         }
 
         protected void Refresh_Click(object sender, EventArgs e)
         {
             TextBoxScore.Text = "";
+        }
+
+        protected void ButtonSave_Click(object sender, EventArgs e)
+        {
+            SaveScore();
+            TextBoxScore.Text = "";
+        }
+
+        void SaveScore()
+        {
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection(_strcon);
+                if (sqlConnection.State == ConnectionState.Closed)
+                {
+                    sqlConnection.Open();
+                }
+
+                string querry;
+                querry = "INSERT INTO GameStatistic(GameName,Username,Score,ScoreDate) values(@gameName,@username,@score,@scoreDate)";
+                SqlCommand sqlCommand = new SqlCommand(querry, sqlConnection);
+                SqlCommand sqlCommandDate = new SqlCommand("SELECT getdate() AS [now]", sqlConnection);
+                SqlDataReader sqlDataReader = sqlCommandDate.ExecuteReader();
+                DateTime time = DateTime.Now;
+                if (sqlDataReader.HasRows)
+                {
+                    while (sqlDataReader.Read())
+                        time = sqlDataReader.GetDateTime(0);
+                }
+                sqlDataReader.Close();
+                string txt = TextBoxScore.Text;
+                sqlCommand.Parameters.AddWithValue("@gameName", "tetris");
+                sqlCommand.Parameters.AddWithValue("@username", Session["username"].ToString());
+                sqlCommand.Parameters.AddWithValue("@score", TextBoxScore.Text.ToString());
+                sqlCommand.Parameters.AddWithValue("@scoreDate", time);
+
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + ".');</script>");
+            }
+        }
+
+        protected void CustomValidatorActice_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if (Session["role"].Equals("user") && Session["status"].Equals("active"))
+            {
+                args.IsValid = true;
+                return;
+            }
+
+            args.IsValid = false;
+            return;
         }
     }
 }
